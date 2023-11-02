c2b644611281b89037acb3b131dcfffd6e22e6ba
Stephen Toub <stoub@microsoft.com>
2023-11-02 09:15:00 -0400

    Focus AI support on OpenAI

--- src/Catalog.API/AIOptions.cs
+++ src/Catalog.API/AIOptions.cs
@@ -2,27 +2,15 @@
 
 public class AIOptions
 {
+    /// <summary>Settings related to the use of OpenAI.</summary>
     public OpenAIOptions OpenAI { get; set; } = new();
-    public VisionOptions Vision { get; set; } = new();
 }
 
 public class OpenAIOptions
 {
-    /// <summary>API key for accessing chat and embedding LLM.</summary>
+    /// <summary>OpenAI API key for accessing embedding LLM.</summary>
     public string APIKey { get; set; }
 
-    /// <summary>Endpoint used for accessing chat and embedding LLM. If it's non-null, it's using Azure OpenAI; if it's null but <see cref="APIKey"/> is non-null, it's using OpenAI.</summary>
-    public string Endpoint { get; set; }
-
-    /// <summary>The name of the embedding model or deployment to use.</summary>
+    /// <summary>The name of the embedding model to use.</summary>
     public string EmbeddingName { get; set; }
-}
-
-public class VisionOptions
-{
-    /// <summary>Optional API key for accessing <see cref="Endpoint"/>.</summary>
-    public string APIKey { get; set; }
-
-    /// <summary>Optional endpoint for accessing Azure vision service, used to generate a description of an image.</summary>
-    public string Endpoint { get; set; }
 }
