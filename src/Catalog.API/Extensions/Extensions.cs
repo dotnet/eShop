4d97cc856da4b503076cef7559d6ffb9ac9bb4c4
Eric Erhardt <eric.erhardt@microsoft.com>
2023-11-07 17:45:40 -0600

    Update to latest aspire preview1 build
    - need to add the stable versions of aspnetcore and efcore, plus their feeds
    - Allow publishing Basket.API and Ordering.BackgroundTasks as Native AOT, and make it warning free by suppressing the 2 known issues that are being fixed. Only PublishAot when a new flag EnableAotPublishing is set. This allows for publishing these 2 projects as normal by default. If someone wants to opt-in to AOT, they just set this flag.

--- src/Catalog.API/Extensions/Extensions.cs
+++ src/Catalog.API/Extensions/Extensions.cs
@@ -4,21 +4,13 @@ public static class Extensions
 {
     public static void AddApplicationServices(this IHostApplicationBuilder builder)
     {
-        // TODO: We need to expose an API for doing this easily as part of the AddNpgsqlDbContext call,
-        // see https://github.com/dotnet/aspire/issues/438. For now, we need to do this manually.
-        // Order is important here as EF will ignore multiple calls to AddDbContext*.
-        builder.Services.AddDbContextPool<CatalogContext>(dbContextOptionsBuilder =>
+        builder.AddNpgsqlDbContext<CatalogContext>("CatalogDB", configureDbContextOptions: dbContextOptionsBuilder =>
         {
             dbContextOptionsBuilder.UseNpgsql(builder =>
             {
-                const int maxRetryCount = 6;
-
-                builder.EnableRetryOnFailure(maxRetryCount);
                 builder.UseVector();
             });
         });
-
-        builder.AddNpgsqlDbContext<CatalogContext>("CatalogDB");
 
         // REVIEW: This is done for development ease but shouldn't be here in production
         builder.Services.AddMigration<CatalogContext, CatalogContextSeed>();
