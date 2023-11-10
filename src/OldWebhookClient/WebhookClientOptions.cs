27e398c76b8715a3177a86a6c7394c81ccb67d27
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2023-11-10 17:19:50 +0000

    Delete old sources

--- src/OldWebhookClient/WebhookClientOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebhookClient;
-
-public class WebhookClientOptions
-{
-    public string Token { get; set; }
-    public string SelfUrl { get; set; }
-    public bool ValidateToken { get; set; }
-}
