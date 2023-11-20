3af2dc80d68980488700d6a5c4549f406893ff00
Jimmy Bogard <jimmy.bogard@gmail.com>
2023-11-20 15:16:34 -0600

    Restoring the event types registration; removing unused HandlerTypes since it is using keyed services

--- src/EventBus/Extensions/EventBusBuilderExtensions.cs
+++ src/EventBus/Extensions/EventBusBuilderExtensions.cs
@@ -24,8 +24,16 @@ public static class EventBusBuilderExtensions
         // Use keyed services to register multiple handlers for the same event type
         // the consumer can use IKeyedServiceProvider.GetKeyedService<IIntegrationEventHandler>(typeof(T)) to get all
         // handlers for the event type.
-
         eventBusBuilder.Services.AddKeyedTransient<IIntegrationEventHandler, TH>(typeof(T));
+
+        eventBusBuilder.Services.Configure<EventBusSubscriptionInfo>(o =>
+        {
+            // Keep track of all registered event types and their name mapping. We send these event types over the message bus
+            // and we don't want to do Type.GetType, so we keep track of the name mapping here.
+
+            // This list will also be used to subscribe to events from the underlying message broker implementation.
+            o.EventTypes[typeof(T).Name] = typeof(T);
+        });
 
         return eventBusBuilder;
     }
