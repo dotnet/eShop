367f3fe0db28421510b454c78c33119c10cdfd19
Eilon Lipton <Eilon@users.noreply.github.com>
2023-11-20 16:05:58 -0800

    Forward only specific APIs from Mobile BFF to Catalog API

--- src/HybridApp/Services/ProductImageUrlProvider.cs
+++ src/HybridApp/Services/ProductImageUrlProvider.cs
@@ -4,6 +4,6 @@ namespace eShop.HybridApp.Services;
 
 public class ProductImageUrlProvider : IProductImageUrlProvider
 {
-    public string GetProductImageUrl(int productId) 
+    public string GetProductImageUrl(int productId)
         => $"{MauiProgram.MobileBffCatalogBaseUrl}api/v1/catalog/items/{productId}/pic";
 }
