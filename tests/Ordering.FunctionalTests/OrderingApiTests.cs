0f732b2fee847b973c4e71a8eecf92af93f0fce9
jflaga <flaga.jeremiah@gmail.com>
2023-12-01 00:30:39 +0800

    Add functional test for Create Order Draft happy path
     - Add AutoFixture

--- tests/Ordering.FunctionalTests/OrderingApiTests.cs
+++ tests/Ordering.FunctionalTests/OrderingApiTests.cs
@@ -1,6 +1,8 @@
-using System.Net;
+﻿using System.Net;
 using System.Text;
 using System.Text.Json;
+using AutoFixture;
+using eShop.Ordering.API.Application.Commands;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace eShop.Ordering.FunctionalTests;
@@ -75,6 +77,27 @@ public sealed class OrderingApiTests : IClassFixture<OrderingApiFixture>
         var response = await _httpClient.PutAsync("api/v1/orders/ship", content);
 
         Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task CreateOrderDraftSucceeds()
+    {
+        Fixture fixture = new Fixture();
+        var payload = fixture.Create<CreateOrderDraftCommand>();
+        var content = new StringContent(JsonSerializer.Serialize(payload), UTF8Encoding.UTF8, "application/json")
+        {
+            Headers = { { "x-requestid", Guid.NewGuid().ToString() } }
+        };
+        var response = await _httpClient.PostAsync("api/v1/orders/draft", content);
+
+        var s = await response.Content.ReadAsStringAsync();
+        var responseData = JsonSerializer.Deserialize<OrderDraftDTO>(s, new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Equal(payload.Items.Count(), responseData.OrderItems.Count());
+        Assert.Contains(payload.Items.First().ProductId, responseData.OrderItems.Select(i => i.ProductId));
+        Assert.Contains(payload.Items.Skip(1).First().ProductId, responseData.OrderItems.Select(i => i.ProductId));
+        Assert.Contains(payload.Items.Skip(2).First().ProductId, responseData.OrderItems.Select(i => i.ProductId));
     }
 
     string BuildOrder()
