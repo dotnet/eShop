21a8f4f1564d669abe8145d128bf541a75466353
saiphanish <saiphanish@gmail.com>
2023-12-09 00:49:26 +0530

    Using Records with Primary Constructors instead of Class for Commands / Queries #116

--- src/Ordering.API/Application/Commands/SetStockRejectedOrderStatusCommand.cs
+++ src/Ordering.API/Application/Commands/SetStockRejectedOrderStatusCommand.cs
@@ -1,17 +1,3 @@
 ﻿namespace eShop.Ordering.API.Application.Commands;
 
-public class SetStockRejectedOrderStatusCommand : IRequest<bool>
-{
-
-    [DataMember]
-    public int OrderNumber { get; private set; }
-
-    [DataMember]
-    public List<int> OrderStockItems { get; private set; }
-
-    public SetStockRejectedOrderStatusCommand(int orderNumber, List<int> orderStockItems)
-    {
-        OrderNumber = orderNumber;
-        OrderStockItems = orderStockItems;
-    }
-}
+public record SetStockRejectedOrderStatusCommand(int OrderNumber, List<int> OrderStockItems) : IRequest<bool>;
