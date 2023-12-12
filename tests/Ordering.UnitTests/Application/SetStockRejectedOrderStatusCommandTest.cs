a318d814131614f06f66f875e601f8e6c7e7f5bb
saiphanish <saiphanish@gmail.com>
2023-12-12 12:05:35 +0530

    Added unit tests for SetStockRejectedOrderStatusCommand

---
+++ tests/Ordering.UnitTests/Application/SetStockRejectedOrderStatusCommandTest.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace eShop.Ordering.UnitTests.Application;
+
+public class SetStockRejectedOrderStatusCommandTest
+{
+    [Fact]
+    public void Set_Stock_Rejected_OrderStatusCommand_Check_Serialization()
+    {
+        // Arrange
+        var command = new SetStockRejectedOrderStatusCommand(123, new List<int> { 1, 2, 3 });
+
+        // Act
+        var json = JsonConvert.SerializeObject(command);
+        var deserializedCommand = JsonConvert.DeserializeObject<SetStockRejectedOrderStatusCommand>(json);
+
+        //Assert
+        Assert.Equal(command.OrderNumber, deserializedCommand.OrderNumber);
+
+        //Assert for List<int>
+        Assert.NotNull(deserializedCommand.OrderStockItems);
+        Assert.Equal(command.OrderStockItems.Count, deserializedCommand.OrderStockItems.Count);
+    }
+}
+
