a612715d94367b598f63d90c92393bc94b794a65
Srushti Pasari (MINDTREE LIMITED) (from Dev Box) <v-sruspasari@microsoft.com>
2023-12-27 02:12:35 +0530

    Added functional test for ordering Api

--- tests/Ordering.FunctionalTests/OrderingApiTests.cs
+++ tests/Ordering.FunctionalTests/OrderingApiTests.cs
@@ -1,6 +1,8 @@
-using System.Net;
+﻿using System.Net;
 using System.Text;
 using System.Text.Json;
+using eShop.Ordering.API.Application.Models;
+using eShop.Ordering.API.Application.Queries;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace eShop.Ordering.FunctionalTests;
@@ -16,65 +18,166 @@ public sealed class OrderingApiTests : IClassFixture<OrderingApiFixture>
         _httpClient = _webApplicationFactory.CreateClient();
     }
 
-
     [Fact]
     public async Task GetAllStoredOrdersWorks()
     {
+        // Act
         var response = await _httpClient.GetAsync("api/v1/orders");
-
         var s = await response.Content.ReadAsStringAsync();
         response.EnsureSuccessStatusCode();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
 
     [Fact]
     public async Task CancelWithEmptyGuidFails()
     {
+        // Act
         var content = new StringContent(BuildOrder(), UTF8Encoding.UTF8, "application/json")
         {
             Headers = { { "x-requestid", Guid.Empty.ToString() } }
         };
         var response = await _httpClient.PutAsync("/api/v1/orders/cancel", content);
-
         var s = await response.Content.ReadAsStringAsync();
+
+        // Assert
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
 
     [Fact]
     public async Task CancelNonExistentOrderFails()
     {
+        // Act
         var content = new StringContent(BuildOrder(), UTF8Encoding.UTF8, "application/json")
         {
             Headers = { { "x-requestid", Guid.NewGuid().ToString() } }
         };
         var response = await _httpClient.PutAsync("api/v1/orders/cancel", content);
-
         var s = await response.Content.ReadAsStringAsync();
+
+        // Assert
         Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
     }
 
     [Fact]
     public async Task ShipWithEmptyGuidFails()
     {
+        // Act
         var content = new StringContent(BuildOrder(), UTF8Encoding.UTF8, "application/json")
         {
             Headers = { { "x-requestid", Guid.Empty.ToString() } }
         };
         var response = await _httpClient.PutAsync("api/v1/orders/ship", content);
-
         var s = await response.Content.ReadAsStringAsync();
+
+        // Assert
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
 
     [Fact]
     public async Task ShipNonExistentOrderFails()
     {
+        // Act
         var content = new StringContent(BuildOrder(), UTF8Encoding.UTF8, "application/json")
         {
             Headers = { { "x-requestid", Guid.NewGuid().ToString() } }
         };
         var response = await _httpClient.PutAsync("api/v1/orders/ship", content);
 
+        // Assert
         Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetAllOrdersCardType()
+    {
+        // Act 1
+        var response = await _httpClient.GetAsync("api/v1/orders/cardtypes");
+        var s = await response.Content.ReadAsStringAsync();
+        response.EnsureSuccessStatusCode();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetStoredOrdersWithOrderId()
+    {
+        // Act
+        var response = await _httpClient.GetAsync("api/v1/orders/1");
+        var responseStatus = response.StatusCode;
+
+        // Assert
+        Assert.Equal("NotFound", responseStatus.ToString());
+    }
+
+    [Fact]
+    public async Task AddNewEmptyOrder()
+    {
+        // Act
+        var content = new StringContent(JsonSerializer.Serialize(new Order()), UTF8Encoding.UTF8, "application/json")
+        {
+            Headers = { { "x-requestid", Guid.Empty.ToString() } }
+        };
+        var response = await _httpClient.PostAsync("api/v1/orders", content);
+        var s = await response.Content.ReadAsStringAsync();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task AddNewOrder()
+    {
+        // Act
+        var item = new BasketItem
+        {
+            Id = "1",
+            ProductId = 12,
+            ProductName = "Test",
+            UnitPrice = 10,
+            OldUnitPrice = 9,
+            Quantity = 1,
+            PictureUrl = null
+        };
+        var cardExpirationDate = Convert.ToDateTime("2023-12-22T12:34:24.334Z");
+        var OrderRequest = new CreateOrderRequest("1", "TestUser", null, null, null, null, null, null, "Test User", cardExpirationDate, "test buyer", 1, null, new List<BasketItem> { item });
+        var content = new StringContent(JsonSerializer.Serialize(OrderRequest), UTF8Encoding.UTF8, "application/json")
+        {
+            Headers = { { "x-requestid", Guid.NewGuid().ToString() } }
+        };
+        var response = await _httpClient.PostAsync("api/v1/orders", content);
+        var s = await response.Content.ReadAsStringAsync();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task PostDraftOrder()
+    {
+        // Act
+        var item = new BasketItem
+        {
+            Id = "1",
+            ProductId = 12,
+            ProductName = "Test",
+            UnitPrice = 10,
+            OldUnitPrice = 9,
+            Quantity = 1,
+            PictureUrl = null
+        };
+        var bodyContent = new CustomerBasket("1", new List<BasketItem> { item });
+        var content = new StringContent(JsonSerializer.Serialize(bodyContent), UTF8Encoding.UTF8, "application/json")
+        {
+            Headers = { { "x-requestid", Guid.NewGuid().ToString() } }
+        };
+        var response = await _httpClient.PostAsync("api/v1/orders/draft", content);
+        var s = await response.Content.ReadAsStringAsync();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
 
     string BuildOrder()
