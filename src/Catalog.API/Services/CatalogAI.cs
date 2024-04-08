2f0658ab50dc2bf80d6e2569edc99902b43838ca
Aaron Powell <me@aaron-powell.com>
2024-04-08 14:03:04 +1000

    Removing old embedding generator method as SK Memory handles that now

--- src/Catalog.API/Services/CatalogAI.cs
+++ src/Catalog.API/Services/CatalogAI.cs
@@ -4,30 +4,13 @@ using Pgvector;
 
 namespace eShop.Catalog.API.Services;
 
-public sealed class CatalogAI(IOptions<AIOptions> options, ILogger<CatalogAI> logger, ISemanticTextMemory memory = null, OpenAIClient openAIClient = null) : ICatalogAI
+public sealed class CatalogAI(IOptions<AIOptions> options, ISemanticTextMemory memory = null, OpenAIClient openAIClient = null) : ICatalogAI
 {
     private const string MemoryCollection = "catalogMemory";
     private readonly string _aiEmbeddingModel = options.Value.OpenAI.EmbeddingName ?? "text-embedding-ada-002";
 
     /// <summary>Gets whether the AI system is enabled.</summary>
     public bool IsEnabled { get; } = openAIClient is not null;
-
-    /// <summary>Gets an embedding vector for the specified text.</summary>
-    public async ValueTask<Vector> GetEmbeddingAsync(string text)
-    {
-        if (!IsEnabled)
-        {
-            return null;
-        }
-
-        if (logger.IsEnabled(LogLevel.Information))
-        {
-            logger.LogInformation("Getting embedding for \"{text}\"", text);
-        }
-
-        EmbeddingsOptions options = new(_aiEmbeddingModel, [text]);
-        return new Vector((await openAIClient.GetEmbeddingsAsync(options)).Value.Data[0].Embedding);
-    }
 
     /// <summary>Saves the specified catalog item to memory.</summary>
     public ValueTask<string> SaveToMemoryAsync(CatalogItem item) =>
--- src/Catalog.API/Services/ICatalogAI.cs
+++ src/Catalog.API/Services/ICatalogAI.cs
@@ -1,12 +1,10 @@
 ﻿using Microsoft.SemanticKernel.Memory;
-using Pgvector;
 
 namespace eShop.Catalog.API.Services;
 
 public interface ICatalogAI
 {
     bool IsEnabled { get; }
-    ValueTask<Vector> GetEmbeddingAsync(string text);
     ValueTask<string> SaveToMemoryAsync(CatalogItem item);
     IAsyncEnumerable<MemoryQueryResult> SearchMemoryAsync(string query, int pageSize);
 }
