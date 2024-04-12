4f32ef716126d0a128016f73c46bed13cab10b63
Aditya Mandaleeka <adityam@microsoft.com>
2024-04-11 17:59:37 -0700

    Get playwright working.

--- src/eShop.AppHost/Program.cs
+++ src/eShop.AppHost/Program.cs
@@ -1,5 +1,6 @@
 ﻿using eShop.AppHost;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
@@ -16,17 +17,19 @@ var identityDb = postgres.AddDatabase("identitydb");
 var orderDb = postgres.AddDatabase("orderingdb");
 var webhooksDb = postgres.AddDatabase("webhooksdb");
 
+var launchProfileName = ShouldUseHttpForEndpoints() ? "http" : "https";
+
 // Services
-var identityApi = builder.AddProject<Projects.Identity_API>("identity-api", "https")
+var identityApi = builder.AddProject<Projects.Identity_API>("identity-api", launchProfileName)
     .WithExternalHttpEndpoints()
     .WithReference(identityDb);
 
-var idpHttps = identityApi.GetEndpoint("https");
+var identityEndpoint = identityApi.GetEndpoint(launchProfileName);
 
 var basketApi = builder.AddProject<Projects.Basket_API>("basket-api")
     .WithReference(redis)
     .WithReference(rabbitMq)
-    .WithEnvironment("Identity__Url", idpHttps);
+    .WithEnvironment("Identity__Url", identityEndpoint);
 
 var catalogApi = builder.AddProject<Projects.Catalog_API>("catalog-api")
     .WithReference(rabbitMq)
@@ -35,7 +38,7 @@ var catalogApi = builder.AddProject<Projects.Catalog_API>("catalog-api")
 var orderingApi = builder.AddProject<Projects.Ordering_API>("ordering-api")
     .WithReference(rabbitMq)
     .WithReference(orderDb)
-    .WithEnvironment("Identity__Url", idpHttps);
+    .WithEnvironment("Identity__Url", identityEndpoint);
 
 builder.AddProject<Projects.OrderProcessor>("order-processor")
     .WithReference(rabbitMq)
@@ -47,7 +50,7 @@ builder.AddProject<Projects.PaymentProcessor>("payment-processor")
 var webHooksApi = builder.AddProject<Projects.Webhooks_API>("webhooks-api")
     .WithReference(rabbitMq)
     .WithReference(webhooksDb)
-    .WithEnvironment("Identity__Url", idpHttps);
+    .WithEnvironment("Identity__Url", identityEndpoint);
 
 // Reverse proxies
 builder.AddProject<Projects.Mobile_Bff_Shopping>("mobile-bff")
@@ -57,15 +60,15 @@ builder.AddProject<Projects.Mobile_Bff_Shopping>("mobile-bff")
 // Apps
 var webhooksClient = builder.AddProject<Projects.WebhookClient>("webhooksclient")
     .WithReference(webHooksApi)
-    .WithEnvironment("IdentityUrl", idpHttps);
+    .WithEnvironment("IdentityUrl", identityEndpoint);
 
-var webApp = builder.AddProject<Projects.WebApp>("webapp", "https")
+var webApp = builder.AddProject<Projects.WebApp>("webapp", launchProfileName)
     .WithExternalHttpEndpoints()
     .WithReference(basketApi)
     .WithReference(catalogApi)
     .WithReference(orderingApi)
     .WithReference(rabbitMq)
-    .WithEnvironment("IdentityUrl", idpHttps);
+    .WithEnvironment("IdentityUrl", identityEndpoint);
 
 // set to true if you want to use OpenAI
 bool useOpenAI = false;
@@ -108,14 +111,26 @@ if (useOpenAI)
 }
 
 // Wire up the callback urls (self referencing)
-webApp.WithEnvironment("CallBackUrl", webApp.GetEndpoint("https"));
-webhooksClient.WithEnvironment("CallBackUrl", webhooksClient.GetEndpoint("https"));
+webApp.WithEnvironment("CallBackUrl", webApp.GetEndpoint(launchProfileName));
+webhooksClient.WithEnvironment("CallBackUrl", webhooksClient.GetEndpoint(launchProfileName));
 
 // Identity has a reference to all of the apps for callback urls, this is a cyclic reference
 identityApi.WithEnvironment("BasketApiClient", basketApi.GetEndpoint("http"))
            .WithEnvironment("OrderingApiClient", orderingApi.GetEndpoint("http"))
            .WithEnvironment("WebhooksApiClient", webHooksApi.GetEndpoint("http"))
-           .WithEnvironment("WebhooksWebClient", webhooksClient.GetEndpoint("https"))
-           .WithEnvironment("WebAppClient", webApp.GetEndpoint("https"));
+           .WithEnvironment("WebhooksWebClient", webhooksClient.GetEndpoint(launchProfileName))
+           .WithEnvironment("WebAppClient", webApp.GetEndpoint(launchProfileName));
 
 builder.Build().Run();
+
+// For test use only.
+// Looks for an environment variable that forces the use of HTTP for all the endpoints. We
+// are doing this for ease of running the Playwright tests in CI.
+static bool ShouldUseHttpForEndpoints()
+{
+    const string EnvVarName = "ESHOP_USE_HTTP_ENDPOINTS";
+    var envValue = Environment.GetEnvironmentVariable(EnvVarName);
+
+    // Attempt to parse the environment variable value; return true if it's exactly "1".
+    return int.TryParse(envValue, out int result) && result == 1;
+}
