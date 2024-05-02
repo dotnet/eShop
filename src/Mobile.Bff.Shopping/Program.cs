8f6ae8024c5e97a12253c3d3b077e82ce3ee4756
Srushti Pasari94 <pasarisrushti@gmail.con>
2024-05-03 02:35:19 +0530

    updated Mobile BFF project  to register health check routes

--- src/Mobile.Bff.Shopping/Program.cs
+++ src/Mobile.Bff.Shopping/Program.cs
@@ -1,6 +1,7 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
+builder.AddBasicServiceDefaults();  // Default health checks 
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
