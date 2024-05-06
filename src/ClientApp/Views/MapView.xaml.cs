e22469db4c08e597881e8fe0f4469467cf29e692
Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>
2024-05-06 02:32:08 +0200

    [fix] remove unused Maps ui initialization line (#344)
    Done in declarative code (xaml) already.
    
    Closees #343

--- src/ClientApp/Views/MapView.xaml.cs
+++ src/ClientApp/Views/MapView.xaml.cs
@@ -8,8 +8,6 @@ public partial class MapView : ContentPageBase
     {
         BindingContext = viewModel;
         InitializeComponent();
-
-        var map = new Microsoft.Maui.Controls.Maps.Map(new Microsoft.Maui.Maps.MapSpan(new Location(0, 0), 0,0));
     }
 
     async void Pin_MarkerClicked(System.Object sender, Microsoft.Maui.Controls.Maps.PinClickedEventArgs e)
