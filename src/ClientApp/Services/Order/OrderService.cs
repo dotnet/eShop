676383eb6cae7b3252591edf7bb8b90fdc40c52f
Michael Stonis <michael.stonis@eightbot.com>
2024-05-29 22:35:24 -0500

    Refactored API versioning in service classes
    The API version string has been refactored into a constant across multiple service classes. This change improves code maintainability by centralizing the API version value, making it easier to update in the future. The affected services include CatalogService, FixUriService, and OrderService.

--- src/ClientApp/Services/Order/OrderService.cs
+++ src/ClientApp/Services/Order/OrderService.cs
@@ -11,6 +11,8 @@ namespace eShop.ClientApp.Services.Order;
 public class OrderService : IOrderService
 {
     private const string ApiUrlBase = "api/orders";
+    private const string ApiVersion = "api-version=1.0";
+    
     private readonly IIdentityService _identityService;
     private readonly IRequestProvider _requestProvider;
     private readonly ISettingsService _settingsService;
@@ -32,7 +34,7 @@ public class OrderService : IOrderService
             return;
         }
 
-        var uri = $"{UriHelper.CombineUri(_settingsService.GatewayOrdersEndpointBase, ApiUrlBase)}?api-version=1.0";
+        var uri = $"{UriHelper.CombineUri(_settingsService.GatewayOrdersEndpointBase, ApiUrlBase)}?{ApiVersion}";
 
         var success = await _requestProvider.PostAsync(uri, newOrder, authToken, "x-requestid").ConfigureAwait(false);
     }
@@ -46,7 +48,7 @@ public class OrderService : IOrderService
             return Enumerable.Empty<Models.Orders.Order>();
         }
 
-        var uri = $"{UriHelper.CombineUri(_settingsService.GatewayOrdersEndpointBase, ApiUrlBase)}?api-version=1.0";
+        var uri = $"{UriHelper.CombineUri(_settingsService.GatewayOrdersEndpointBase, ApiUrlBase)}?{ApiVersion}";
 
         var orders =
             await _requestProvider.GetAsync<IEnumerable<Models.Orders.Order>>(uri, authToken).ConfigureAwait(false);
@@ -65,7 +67,7 @@ public class OrderService : IOrderService
 
         try
         {
-            var uri = $"{UriHelper.CombineUri(_settingsService.GatewayOrdersEndpointBase, $"{ApiUrlBase}/{orderId}")}?api-version=1.0";
+            var uri = $"{UriHelper.CombineUri(_settingsService.GatewayOrdersEndpointBase, $"{ApiUrlBase}/{orderId}")}?{ApiVersion}";
 
             var order =
                 await _requestProvider.GetAsync<Models.Orders.Order>(uri, authToken).ConfigureAwait(false);
@@ -87,7 +89,7 @@ public class OrderService : IOrderService
             return false;
         }
 
-        var uri = $"{UriHelper.CombineUri(_settingsService.GatewayOrdersEndpointBase, $"{ApiUrlBase}/cancel")}?api-version=1.0";
+        var uri = $"{UriHelper.CombineUri(_settingsService.GatewayOrdersEndpointBase, $"{ApiUrlBase}/cancel")}?{ApiVersion}";
 
         var cancelOrderCommand = new CancelOrderCommand(orderId);
 
