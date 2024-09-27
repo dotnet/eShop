d10911ba476304d587060ddfb08ab5a5588513f6
Safia Abdalla <safia@microsoft.com>
2024-09-26 17:25:46 -0700

    Update to .NET 9 (#532)
    * Update to .NET 9
    
    Update eShop to use built-in OpenAPI support (#360)
    
    * Update eShop to use built-in OpenAPI support
    
    * Pass version names explicitly
    
    Update dependencies
    
    Downgrade EF Core, upgrade SDK, freeze workloads
    
    Upgrade to working efcore and rename rollback file
    
    Rearrange items in Directory.Packages.props
    
    * Update Microsoft.Extensions.Http.Resilience dependency
    
    * Remove unneeded OpenAPI workarounds
    
    * Address feedback

--- src/eShop.ServiceDefaults/OpenApi.Extensions.cs
+++ src/eShop.ServiceDefaults/OpenApi.Extensions.cs
@@ -1,11 +1,12 @@
 ﻿using Asp.Versioning;
+using Asp.Versioning.ApiExplorer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
-using Swashbuckle.AspNetCore.SwaggerGen;
+using Scalar.AspNetCore;
 
 namespace eShop.ServiceDefaults;
 
@@ -21,45 +22,12 @@ public static partial class Extensions
             return app;
         }
 
-        app.UseSwagger();
+        app.MapOpenApi();
 
         if (app.Environment.IsDevelopment())
         {
-            app.UseSwaggerUI(setup =>
-            {
-                /// {
-                ///   "OpenApi": {
-                ///     "Endpoint: {
-                ///         "Name": 
-                ///     },
-                ///     "Auth": {
-                ///         "ClientId": ..,
-                ///         "AppName": ..
-                ///     }
-                ///   }
-                /// }
-
-                var pathBase = configuration["PATH_BASE"] ?? string.Empty;
-                var authSection = openApiSection.GetSection("Auth");
-                var endpointSection = openApiSection.GetRequiredSection("Endpoint");
-
-                foreach (var description in app.DescribeApiVersions())
-                {
-                    var name = description.GroupName;
-                    var url = endpointSection["Url"] ?? $"{pathBase}/swagger/{name}/swagger.json";
-
-                    setup.SwaggerEndpoint(url, name);
-                }
-
-                if (authSection.Exists())
-                {
-                    setup.OAuthClientId(authSection.GetRequiredValue("ClientId"));
-                    setup.OAuthAppName(authSection.GetRequiredValue("AppName"));
-                }
-            });
-
-            // Add a redirect from the root of the app to the swagger endpoint
-            app.MapGet("/", () => Results.Redirect("/swagger")).ExcludeFromDescription();
+            app.MapScalarApiReference();
+            app.MapGet("/", () => Results.Redirect("/scalar/v1")).ExcludeFromDescription();
         }
 
         return app;
@@ -69,24 +37,35 @@ public static partial class Extensions
         this IHostApplicationBuilder builder,
         IApiVersioningBuilder? apiVersioning = default)
     {
-        var services = builder.Services;
-        var configuration = builder.Configuration;
-        var openApi = configuration.GetSection("OpenApi");
+        var openApi = builder.Configuration.GetSection("OpenApi");
+        var identitySection = builder.Configuration.GetSection("Identity");
+
+        var scopes = identitySection.Exists()
+            ? identitySection.GetRequiredSection("Scopes").GetChildren().ToDictionary(p => p.Key, p => p.Value)
+            : new Dictionary<string, string?>();
+
 
         if (!openApi.Exists())
         {
             return builder;
         }
 
-        services.AddEndpointsApiExplorer();
-
         if (apiVersioning is not null)
         {
             // the default format will just be ApiVersion.ToString(); for example, 1.0.
             // this will format the version as "'v'major[.minor][-status]"
-            apiVersioning.AddApiExplorer(options => options.GroupNameFormat = "'v'VVV");
-            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
-            services.AddSwaggerGen(options => options.OperationFilter<OpenApiDefaultValues>());
+            var versioned = apiVersioning.AddApiExplorer(options => options.GroupNameFormat = "'v'VVV");
+            string[] versions = ["v1"];
+            foreach (var description in versions)
+            {
+                builder.Services.AddOpenApi(description, options =>
+                {
+                    options.ApplyApiVersionInfo(openApi.GetRequiredValue("Document:Title"), openApi.GetRequiredValue("Document:Description"));
+                    options.ApplyAuthorizationChecks([.. scopes.Keys]);
+                    options.ApplySecuritySchemeDefinitions();
+                    options.ApplyOperationDeprecatedStatus();
+                });
+            }
         }
 
         return builder;
---
+++ src/eShop.ServiceDefaults/OpenApiOptionsExtensions.cs
@@ -0,0 +1,195 @@
+﻿using System.Text;
+using Asp.Versioning.ApiExplorer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Primitives;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+
+namespace eShop.ServiceDefaults;
+
+internal static class OpenApiOptionsExtensions
+{
+    public static OpenApiOptions ApplyApiVersionInfo(this OpenApiOptions options, string title, string description)
+    {
+        options.AddDocumentTransformer((document, context, cancellationToken) =>
+        {
+            var versionedDescriptionProvider = context.ApplicationServices.GetService<IApiVersionDescriptionProvider>();
+            var apiDescription = versionedDescriptionProvider?.ApiVersionDescriptions
+                .SingleOrDefault(description => description.GroupName == context.DocumentName);
+            if (apiDescription is null)
+            {
+                return Task.CompletedTask;
+            }
+            document.Info.Version = apiDescription.ApiVersion.ToString();
+            document.Info.Title = title;
+            document.Info.Description = BuildDescription(apiDescription, description);
+            return Task.CompletedTask;
+        });
+        return options;
+    }
+
+    private static string BuildDescription(ApiVersionDescription api, string description)
+    {
+        var text = new StringBuilder(description);
+
+        if (api.IsDeprecated)
+        {
+            if (text.Length > 0)
+            {
+                if (text[^1] != '.')
+                {
+                    text.Append('.');
+                }
+
+                text.Append(' ');
+            }
+
+            text.Append("This API version has been deprecated.");
+        }
+
+        if (api.SunsetPolicy is { } policy)
+        {
+            if (policy.Date is { } when)
+            {
+                if (text.Length > 0)
+                {
+                    text.Append(' ');
+                }
+
+                text.Append("The API will be sunset on ")
+                    .Append(when.Date.ToShortDateString())
+                    .Append('.');
+            }
+
+            if (policy.HasLinks)
+            {
+                text.AppendLine();
+
+                var rendered = false;
+
+                foreach (var link in policy.Links.Where(l => l.Type == "text/html"))
+                {
+                    if (!rendered)
+                    {
+                        text.Append("<h4>Links</h4><ul>");
+                        rendered = true;
+                    }
+
+                    text.Append("<li><a href=\"");
+                    text.Append(link.LinkTarget.OriginalString);
+                    text.Append("\">");
+                    text.Append(
+                        StringSegment.IsNullOrEmpty(link.Title)
+                        ? link.LinkTarget.OriginalString
+                        : link.Title.ToString());
+                    text.Append("</a></li>");
+                }
+
+                if (rendered)
+                {
+                    text.Append("</ul>");
+                }
+            }
+        }
+
+        return text.ToString();
+    }
+
+    public static OpenApiOptions ApplySecuritySchemeDefinitions(this OpenApiOptions options)
+    {
+        options.AddDocumentTransformer<SecuritySchemeDefinitionsTransformer>();
+        return options;
+    }
+
+    public static OpenApiOptions ApplyAuthorizationChecks(this OpenApiOptions options, string[] scopes)
+    {
+        options.AddOperationTransformer((operation, context, cancellationToken) =>
+        {
+            var metadata = context.Description.ActionDescriptor.EndpointMetadata;
+
+            if (!metadata.OfType<IAuthorizeData>().Any())
+            {
+                return Task.CompletedTask;
+            }
+
+            operation.Responses.TryAdd("401", new OpenApiResponse { Description = "Unauthorized" });
+            operation.Responses.TryAdd("403", new OpenApiResponse { Description = "Forbidden" });
+
+            var oAuthScheme = new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
+            };
+
+            operation.Security = new List<OpenApiSecurityRequirement>
+            {
+                new()
+                {
+                    [oAuthScheme] = scopes
+                }
+            };
+
+            return Task.CompletedTask;
+        });
+        return options;
+    }
+
+    public static OpenApiOptions ApplyOperationDeprecatedStatus(this OpenApiOptions options)
+    {
+        options.AddOperationTransformer((operation, context, cancellationToken) =>
+        {
+            var apiDescription = context.Description;
+            operation.Deprecated |= apiDescription.IsDeprecated();
+            return Task.CompletedTask;
+        });
+        return options;
+    }
+
+    private static IOpenApiAny? CreateOpenApiAnyFromObject(object value)
+    {
+        return value switch
+        {
+            bool b => new OpenApiBoolean(b),
+            int i => new OpenApiInteger(i),
+            double d => new OpenApiDouble(d),
+            string s => new OpenApiString(s),
+            _ => null
+        };
+    }
+
+    private class SecuritySchemeDefinitionsTransformer(IConfiguration configuration) : IOpenApiDocumentTransformer
+    {
+        public Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
+        {
+            var identitySection = configuration.GetSection("Identity");
+            if (!identitySection.Exists())
+            {
+                return Task.CompletedTask;
+            }
+
+            var identityUrlExternal = identitySection.GetRequiredValue("Url");
+            var scopes = identitySection.GetRequiredSection("Scopes").GetChildren().ToDictionary(p => p.Key, p => p.Value);
+            var securityScheme = new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.OAuth2,
+                Flows = new OpenApiOAuthFlows()
+                {
+                    // TODO: Change this to use Authorization Code flow with PKCE
+                    Implicit = new OpenApiOAuthFlow()
+                    {
+                        AuthorizationUrl = new Uri($"{identityUrlExternal}/connect/authorize"),
+                        TokenUrl = new Uri($"{identityUrlExternal}/connect/token"),
+                        Scopes = scopes,
+                    }
+                }
+            };
+            document.Components ??= new();
+            document.Components.SecuritySchemes.Add("oauth2", securityScheme);
+            return Task.CompletedTask;
+        }
+    }
+}
