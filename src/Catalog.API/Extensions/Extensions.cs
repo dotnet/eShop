f46f56fa09add492c63c26f68fea91dcc1b1c907
Eric Erhardt <eric.erhardt@microsoft.com>
2024-10-16 12:02:00 -0500

    Update to .NET Aspire 9.0 (#567)
    * aspire 9 first pass
    
    * Update to latest versions and clean up
    
    * Add WaitFor and Persistent container lifetimes
    
    * Update with Microsoft.Extensions.AI
    
    * Avoid hardcoding url and remove explicit transitive dependency
    
    * Enable chat to work with Ollama
    
    * Fix Azure OpenAI settings
    
    * PR feedback
    
    - turn off openai
    - use rc2 SDK
    
    * Enable prompts/completions in OTel events
    
    * Refactor OpenAI code out of Main and into its own extension method.
    
    * Update to latest Aspire version
    
    * Update SDK in projects
    
    * Update to latest Aspire rc.1 build
    
    * Clean up in preparation for merging to main.
    
    ---------
    
    Co-authored-by: Maddy Montaquila <maddyleger1@gmail.com>
    Co-authored-by: Stephen Toub <stoub@microsoft.com>
    Co-authored-by: Damian Edwards <damian@damianedwards.com>

--- src/Catalog.API/Extensions/Extensions.cs
+++ src/Catalog.API/Extensions/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using eShop.Catalog.API.Services;
-using Microsoft.SemanticKernel;
+using Microsoft.Extensions.AI;
+using OpenAI;
 
 public static class Extensions
 {
@@ -28,17 +29,24 @@ public static class Extensions
         builder.Services.AddOptions<CatalogOptions>()
             .BindConfiguration(nameof(CatalogOptions));
 
-        if (builder.Configuration["AI:Onnx:EmbeddingModelPath"] is string modelPath &&
-            builder.Configuration["AI:Onnx:EmbeddingVocabPath"] is string vocabPath)
+        if (builder.Configuration["AI:Ollama:Endpoint"] is string ollamaEndpoint && !string.IsNullOrWhiteSpace(ollamaEndpoint))
         {
-            builder.Services.AddBertOnnxTextEmbeddingGeneration(modelPath, vocabPath);
+            builder.Services.AddEmbeddingGenerator<string, Embedding<float>>(b => b
+                .UseOpenTelemetry()
+                .UseLogging()
+                .Use(new OllamaEmbeddingGenerator(
+                    new Uri(ollamaEndpoint),
+                    builder.Configuration["AI:Ollama:EmbeddingModel"])));
         }
         else if (!string.IsNullOrWhiteSpace(builder.Configuration.GetConnectionString("openai")))
         {
-            builder.AddAzureOpenAIClient("openai");
-            builder.Services.AddOpenAITextEmbeddingGeneration(builder.Configuration["AIOptions:OpenAI:EmbeddingName"] ?? "text-embedding-3-small");
+            builder.AddOpenAIClientFromConfiguration("openai");
+            builder.Services.AddEmbeddingGenerator<string, Embedding<float>>(b => b
+                .UseOpenTelemetry()
+                .UseLogging()
+                .Use(b.Services.GetRequiredService<OpenAIClient>().AsEmbeddingGenerator(builder.Configuration["AI:OpenAI:EmbeddingModel"]!)));
         }
 
-        builder.Services.AddSingleton<ICatalogAI, CatalogAI>();
+        builder.Services.AddScoped<ICatalogAI, CatalogAI>();
     }
 }
