5f5c8be418ee1093735d509f4f12b541d98eb9e5
Mike Kistler <mikekistler@microsoft.com>
2025-01-06 21:22:17 -0600

    Update Catalog clients to V2 API version

--- src/HybridApp/Services/ProductImageUrlProvider.cs
+++ src/HybridApp/Services/ProductImageUrlProvider.cs
@@ -5,5 +5,5 @@ namespace eShop.HybridApp.Services;
 public class ProductImageUrlProvider : IProductImageUrlProvider
 {
     public string GetProductImageUrl(int productId)
-        => $"{MauiProgram.MobileBffHost}api/catalog/items/{productId}/pic?api-version=1.0";
+        => $"{MauiProgram.MobileBffHost}api/catalog/items/{productId}/pic?api-version=2.0";
 }
