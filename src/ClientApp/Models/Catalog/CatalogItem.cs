7e64cd0504cde33783e7a9430f78b2ed637a0c8a
Şhaʤ <rimt07@gmail.com>
2025-04-23 16:09:18 -0600

    Update Product Model to add promotional discount fields

--- src/ClientApp/Models/Catalog/CatalogItem.cs
+++ src/ClientApp/Models/Catalog/CatalogItem.cs
@@ -4,7 +4,6 @@ public class CatalogItem
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    
     public string Description { get; set; }
     public decimal Price { get; set; }
     public string PictureUri { get; set; }
@@ -12,4 +11,10 @@ public class CatalogItem
     public CatalogBrand CatalogBrand { get; set; }
     public int CatalogTypeId { get; set; }
     public CatalogType CatalogType { get; set; }
+    
+    public int? PromotionalDiscountPercentage { get; set; }
+    public string PromotionalDiscountDescription { get; set; }
+    public DateTime? PromotionStartDate { get; set; }
+    public DateTime? PromotionEndDate { get; set; }
+    public string BadgeDesign { get; set; }
 }
