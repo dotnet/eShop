01d2a6aba60033a710bce2be018e9d6574dcb8a7
Jose Perez Rodriguez <joperezr@microsoft.com>
2025-09-04 08:36:27 -0700

    * 'Line 6: Removed the using directive for 'Microsoft.OpenApi.Any' as the namespace or type 'Any' does not exist in 'Microsoft.OpenApi' in .NET 10.0.' in file 'src\eShop.ServiceDefaults\OpenApiOptionsExtensions.cs'

--- src/eShop.ServiceDefaults/OpenApiOptionsExtensions.cs
+++ src/eShop.ServiceDefaults/OpenApiOptionsExtensions.cs
@@ -3,7 +3,6 @@ using Asp.Versioning.ApiExplorer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.OpenApi.Models;
-using Microsoft.OpenApi.Any;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
